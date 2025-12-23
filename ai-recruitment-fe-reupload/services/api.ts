654650ad64da1b0421879d7d9ab6debe155e5ba7
nailathaya <athhayamumtaz@gmail.com>
2025-12-24 01:18:32 +0700

    setup env for deployment

--- ai-recruitment-fe-reupload/services/api.ts
+++ ai-recruitment-fe-reupload/services/api.ts
@@ -1,7 +1,7 @@
 // services/api.ts
 import { WorkExperience, Education, Skill, Document } from "../types";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 /* =====================
    AUTH HEADER
